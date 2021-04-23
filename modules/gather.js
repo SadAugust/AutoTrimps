4ba67cff762d3a2cc565929a6ade937b584b91e5
SadAugust <meandalena@gmail.com>
2021-04-23 20:54:37 +0100

    Update gather.js

--- modules/gather.js
+++ modules/gather.js
@@ -178,6 +178,9 @@ function RmanualLabor2() {
         }
 	return;
     }
+	if (game.global.challengeActive == "Pandemonium" && !Rshouldpandemonium && getPageSetting('RequipPandemonium') && getPageSetting('RPandemoniumEqStaff') != "undefined" && getPageSetting('RPandemoniumEqLvl') > 0 && game.global.lastClearedCell > 59) {
+		setGather('metal');
+	}
 	var rspecial = '?';
 	if ((Rshouldtimefarm || Rshouldalchfarm) && game.global.mapsActive) {
 		if (Rshouldalchfarm) {
