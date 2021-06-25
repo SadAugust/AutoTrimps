fc9f51e39426199076c49e5e92c8495b191a5ffd
SadAugust <meandalena@gmail.com>
2021-06-25 12:27:33 +0100

    Update heirlooms.js

--- modules/heirlooms.js
+++ modules/heirlooms.js
@@ -286,9 +286,8 @@ function HeirloomSwapping() {
         }
 
         //Swapping Staffs
-      //Will get used after implementing SimpleSeconds heirloom swapper  
-      //if (Rshouldpandemoniumfarm && getPageSetting('RhsPandStaff') != "undefined") {
-        if (game.global.challengeActive == "Pandemonium" && getPageSetting('RPandemoniumAutoEquip') > 1 && getPageSetting('RhsPandStaff') != "undefined" && getPageSetting('RPandemoniumAEZone') > 0 && game.global.world >= getPageSetting('RPandemoniumAEZone') && game.global.lastClearedCell > 59) {
+        if ((Rshouldpandemoniumfarm || (!game.global.mapsActive && game.global.challengeActive == "Pandemonium")) && getPageSetting('RhsPandStaff') != "undefined") {
+        //if (game.global.challengeActive == "Pandemonium" && getPageSetting('RPandemoniumAutoEquip') > 1 && getPageSetting('RhsPandStaff') != "undefined" && getPageSetting('RPandemoniumAEZone') > 0 && game.global.world >= getPageSetting('RPandemoniumAEZone') && game.global.lastClearedCell > 59) {
             HeirloomEquipStaff('RhsPandStaff');
         } else if (getPageSetting('RhsStaff')) {
             if (getPageSetting('RhsWorldStaff') != "undefined" && !game.global.mapsActive) {
