db4b837ce42c6255396d65ff446d6573ea4ccbf2
SadAugust <meandalena@gmail.com>
2022-03-27 22:23:10 +0100

    Add files via upload

---
+++ mods.js
@@ -0,0 +1,5 @@
+var script = document.createElement('script');
+script.id = 'AutoTrimps-Zek';
+script.src = 'https://SadAugust.github.io/AutoTrimps_Local/AutoTrimps2.js';
+script.setAttribute('crossorigin',"anonymous");
+document.head.appendChild(script);
