a17651bf80ec992a516abfe6e662f7cd34ebd3ea
SadAugust <meandalena@gmail.com>
2022-11-18 21:18:59 +0000

    Revert "Merge branch 'main' of https://github.com/SadAugust/AutoTrimps_Local"
    This reverts commit 9af008c21d660e2d8485f8815416af3b286e112a, reversing
    changes made to 149e7e07b49cb46555479f685cd8d966221c5e84.

--- modules/portal.js
+++ modules/portal.js
@@ -648,12 +648,14 @@ function Rresetmapvars() {
 	rHDFMapRepeats = 0;
 
 	//Resetting variables that would cause issues if they were left as is
-	MODULES.mapFunctions.rVoidHDRatio = Infinity;
-	MODULES.mapFunctions.rVoidVHDRatio = Infinity;
-	MODULES.mapFunctions.rVoidHDIndex = Infinity;
-	MODULES.mapFunctions.rPortalZone = Infinity;
-	HDRatio = game.global.universe === 1 ? calcHDratio() : RcalcHDratio();
-	voidHDRatio = game.global.universe === 1 ? calcVoidHDratio() : rCalcVoidHDratio();
+	if (game.global.universe === 2) {
+		MODULES.mapFunctions.rVoidHDRatio = Infinity;
+		MODULES.mapFunctions.rVoidVHDRatio = Infinity;
+		MODULES.mapFunctions.rVoidHDIndex = Infinity;
+		MODULES.mapFunctions.rPortalZone = Infinity;
+		HDRatio = RcalcHDratio();
+		voidHDRatio = rCalcVoidHDratio()
+	}
 
 	if (document.getElementById('hiderStatus').style.display == 'None' && getPageSetting('Rshowrnhr') && !game.global.runningChallengeSquared) {
 		turnOn("hiderStatus")
