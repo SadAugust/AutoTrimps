e0976b98131cddc1a6a0955cd75b883c07b5619e
SadAugust <meandalena@gmail.com>
2022-12-18 15:12:40 +0000

    Enhance Map Grid bugfix

--- modules/fight-info.js
+++ modules/fight-info.js
@@ -3,177 +3,171 @@
 	M["fightinfo"].$worldGrid = document.getElementById('grid');
 	M["fightinfo"].$mapGrid = document.getElementById('mapGrid');
 
-	// Powerful imps
-	M["fightinfo"].powerful =
-		[
-			"Improbability",
-			"Omnipotrimp",
-			"Mutimp",
-			"Hulking_Mutimp"
-		];
+	//This changes the colour of the cell. It's usually bad, because it overrides trimps and looks bad against corruption, among other reasons
+	M["fightinfo"].changeCellColor = false;
 
-	// Exotic imps
-	M["fightinfo"].exotics =
-		[
-			"Feyimp",
-			"Tauntimp",
-			"Venimp",
-			"Whipimp",
-			"Magnimp",
-			"Goblimp",
-			"Flutimp",
-			"Jestimp",
-			"Titimp",
-			"Chronoimp"
-		];
+	//This option reverts to AT's old way of giving an unique icon for each of the exotic/powerful imps
+	M["fightinfo"].allExoticIcons = true;
+	M["fightinfo"].allPowerfulIcons = true;
 
-	// Fast imps
-	M["fightinfo"].fast =
-		[
-			"Snimp",
-			"Kittimp",
-			"Gorillimp",
-			"Squimp",
-			"Shrimp",
-			"Chickimp",
-			"Frimp",
-			"Slagimp",
-			"Lavimp",
-			"Kangarimp",
-			"Entimp",
-			"Carbimp",
-		];
+	M["fightinfo"].imp = {
+		skel: { icon: '"glyphicon glyphicon-italic"', shadow: "0px 0px 10px #ffffff", color: '#ffffff' },
+		exotic: { icon: '"glyphicon glyphicon-sunglasses"', shadow: "0px 0px 10px #fb753f", color: '#ff0000' },
+		powerful: { icon: '"glyphicon glyphicon-fire"', shadow: "0px 0px 10px #ff0c55", color: '#ff0c55' },
+		fast: { icon: '"glyphicon glyphicon-forward"', shadow: "0px 0px 10px #ffffff", color: '#666666' },
+		poison: { icon: '"glyphicon glyphicon-flask"', shadow: "0px 0px 10px #ffffff", color: '#00ff00' },
+		wind: { icon: '"icomoon icon-air"', shadow: "0px 0px 10px #ffffff", color: '#99ffff' },
+		ice: { icon: '"glyphicon glyphicon-certificate"', shadow: "0px 0px 10px #ffffff", color: '#00ffff' }
+	};
 
-	//Colors for special imps (This has been disabled)
-	M["fightinfo"].colors =
-	{
-		bone: '#ffffff',
-		exotic: '#000000',
-		powerful: '#000000',
-		fast: '#000000'
-	}
+	//Powerful imps
+	M["fightinfo"].powerful = {
+		blimp: { name: "Blimp", icon: '"glyphicon glyphicon-plane"' },
+		cthulimp: { name: "Cthulimp", icon: '"icomoon icon-archive"' },
+		improbability: { name: "Improbability", icon: '"glyphicon glyphicon-question-sign"' },
+		omnipotrimp: { name: "Omnipotrimp", icon: '"glyphicon glyphicon-fire"' },
+		mutimp: { name: "Mutimp", icon: '"glyphicon glyphicon-menu-up"' },
+		hulking_mutimp: { name: "Hulking_Mutimp", icon: '" glyphicon glyphicon-chevron-up"' }
+	};
 
+	//Exotic imps
+	M["fightinfo"].exotics = {
+		chronoimp: { name: "Chronoimp", icon: '"glyphicon glyphicon-hourglass"' },
+		feyimp: { name: "Feyimp", icon: '"icomoon icon-diamond"' },
+		flutimp: { name: "Flutimp", icon: '"glyphicon glyphicon-globe"' },
+		goblimp: { name: "Goblimp", icon: '"icomoon icon-evil"' },
+		jestimp: { name: "Jestimp", icon: '"icomoon icon-mask"' },
+		magnimp: { name: "Magnimp", icon: '"glyphicon glyphicon-magnet"' },
+		tauntimp: { name: "Tauntimp", icon: '"glyphicon glyphicon-tent"' },
+		titimp: { name: "Titimp", icon: '"icomoon icon-hammer"' },
+		venimp: { name: "Venimp", icon: '"glyphicon glyphicon-baby-formula"' },
+		whipimp: { name: "Whipimp", icon: '"icomoon icon-area-graph"' },
+	};
+
+	//Fast imps
+	M["fightinfo"].fast = [
+		"Squimp",
+		"Snimp",
+		"Gorillimp",
+		"Shrimp",
+		"Chickimp",
+		"Kittimp",
+		"Frimp",
+		"Slagimp",
+		"Lavimp",
+		"Kangarimp",
+		"Entimp",
+		"Fusimp",
+		"Carbimp",
+		"Shadimp",
+		"Voidsnimp",
+		"Prisimp",
+		"Sweltimp",
+		"Horrimp",
+	];
+
+	//Last processed
 	M["fightinfo"].lastProcessedWorld = null;
 	M["fightinfo"].lastProcessedMap = null;
 
+	function identifyCell(cell) {//TODO What was this for anyway? I forgot. I think it was needed to fix nature icons?
+		//Init
+		var tags = [];
+		var name = cell.name.toLowerCase();
+
+		//Skeletimp
+		if (name.includes("skeletimp")) tags.push("skeletimp");
+
+		//Exotics
+		if (name in M.fightinfo.exotics) tags.push("exotic")
+
+		//Nature
+		if (name.startsWith("poison")) tags.push("poison");
+		if (name.startsWith("wind")) tags.push("wind");
+		if (name.startsWith("ice")) tags.push("ice");
+	}
+
+	function updateCell($cell, cell, pallet, customIcon, overrideSpecial, overrideCoords) {
+		//Cell Color
+		if (M.fightinfo.changeCellColor) $cell.style.color = pallet.color;
+		$cell.style.textShadow = pallet.shadow;
+
+		//Glyph Icon
+		var icon = (customIcon) ? customIcon : pallet.icon
+		var replaceable = ["fruit", "Metal", "gems", "freeMetals", "groundLumber", "Wood", "Map", "Any"]
+		if (overrideCoords) replaceable.push("Coordination");
+
+		//Icon Overriding
+		if (cell.special.length == 0 || overrideSpecial && replaceable.includes(cell.special))
+			$cell.innerHTML = "<span class=" + icon + "></span>";
+	}
+
 	function Update() {
-		// Check if we should update world or map info
-		if (game.global.mapsActive) {
-			// Check if current map already infoed
-			// Can't do this because of map repeating
-			/*if(M["fightinfo"].lastProcessedMap === null || M["fightinfo"].lastProcessedMap !== game.global.lookingAtMap)
-				M["fightinfo"].lastProcessedMap = game.global.lookingAtMap;
-			else
-				return;*/
-
-			// Cell data
-			var cells = game.global.mapGridArray;
-
-			// DOM rows
-			var $rows = Array.prototype.slice.call(M["fightinfo"].$mapGrid.children);
-		}
-		else {
-			// Check if current world already infoed
-			if (M["fightinfo"].lastProcessedWorld === null || M["fightinfo"].lastProcessedWorld !== game.global.world)
-				M["fightinfo"].lastProcessedWorld = game.global.world;
-			else
-				return;
-
-			// Cell data
-			var cells = game.global.gridArray;
-
-			// DOM rows
-			var $rows = Array.prototype.slice.call(M["fightinfo"].$worldGrid.children);
-		}
-
-		// Rows are in inverse order somewhy
-		$rows = $rows.reverse();
-
-		// DOM cells
+		//Check if we should update world or map info
 		var $cells = [];
+		var cells = (game.global.mapsActive) ? game.global.mapGridArray : game.global.gridArray;
+		var rowSource = (game.global.mapsActive) ? M["fightinfo"].$mapGrid.children : M["fightinfo"].$worldGrid.children;
+		var $rows = Array.prototype.slice.call(rowSource).reverse();
 
-		// Loop through DOM rows and concat each row's cell-element into $cells
-		$rows.forEach(function (x) {
-			$cells = $cells.concat(Array.prototype.slice.call(x.children));
+		//Check if current the world is already info-ed
+		if (!game.global.mapsActive && M["fightinfo"].lastProcessedWorld == game.global.world)
+			return;
+
+		//Set this world as info-ed
+		else if (!game.global.mapsActive) M["fightinfo"].lastProcessedWorld = game.global.world;
+
+		//Loop through DOM rows and concat each row's cell-element into $cells
+		$rows.forEach(function (row) {
+			$cells = $cells.concat(Array.prototype.slice.call(row.children));
 		});
 
-		// Process all cells
+		//Process all cells
 		for (var i = 0; i < $cells.length; i++) {
-			// DOM cell
+			//Init
 			var $cell = $cells[i];
-
-			// Cell data
 			var cell = cells[i];
 
-
-			if (cell.name.toLowerCase().indexOf('skele') > -1)					// Skeletimp cell
-			{
-				if (cell.special.length === 0)
-					$cell.innerHTML = "<span class=\"glyphicon glyphicon-italic\"></span> ";
-
-				$cell.title = cell.name;
-				//$cell.style.color = M["fightinfo"].colors.bone; //(This changes the colour of the glyph - bad bc it overrides trimps and looks bad against corruption etc)
-				$cell.style.textShadow = '0px 0px 10px #ffffff';
+			//Skeletimp
+			if (cell.name.toLowerCase().indexOf('skele') > -1) {
+				updateCell($cell, cell, M.fightinfo.imp.skel);
 			}
 
-			else if (M["fightinfo"].exotics.indexOf(cell.name) > -1)				// Exotic cell
-			{
-				if (cell.special.length === 0)
-					$cell.innerHTML = "<span class=\"glyphicon glyphicon-sunglasses\"></span> ";
-
-				$cell.title = cell.name;
-				//$cell.style.color = M["fightinfo"].colors.exotic; //(This changes the colour of the glyph - bad bc it overrides trimps and looks bad against corruption etc)
-				$cell.style.textShadow = '0px 0px 10px #fb753f';
+			//Exotic cell
+			else if (cell.name.toLowerCase() in M["fightinfo"].exotics) {
+				let icon = M.fightinfo.allExoticIcons ? M.fightinfo.exotics[cell.name.toLowerCase()].icon : undefined;
+				updateCell($cell, cell, M.fightinfo.imp.exotic, icon, true);
 			}
 
-			else if (M["fightinfo"].powerful.indexOf(cell.name) > -1)			// Powerful imp
-			{
-				if (cell.special.length === 0)
-					$cell.innerHTML = "<span class=\"glyphicon glyphicon-hazard\"></span> ";
-
-				$cell.title = cell.name;
-				//$cell.style.color = M["fightinfo"].colors.powerful; //(This changes the colour of the glyph - bad bc it overrides trimps and looks bad against corruption etc)
-				$cell.style.textShadow = '0px 0px 10px #8c0000';
+			//Powerful Imp
+			else if (cell.name.toLowerCase() in M["fightinfo"].powerful) {
+				let icon = M.fightinfo.allPowerfulIcons ? M.fightinfo.powerful[cell.name.toLowerCase()].icon : undefined;
+				updateCell($cell, cell, M.fightinfo.imp.powerful, icon, M.fightinfo.allPowerfulIcons, true);
 			}
 
-			else if (M["fightinfo"].fast.indexOf(cell.name) > -1)				// Fast imp
-			{
-				//if(cell.special.length === 0)
-				$cell.innerHTML = "<span class=\"glyphicon glyphicon-forward\"></span> ";
-
-				$cell.title = cell.name;
-				//$cell.style.color = M["fightinfo"].colors.fast; //(This changes the colour of the glyph - bad bc it overrides trimps and looks bad against corruption etc)
-				$cell.style.textShadow = '0px 0px 10px #ffffff';
+			//Fast Imp
+			else if (M["fightinfo"].fast.indexOf(cell.name) > -1 && (!cell.corrupted || !cell.corrupted.startsWith("corrupt"))) {
+				updateCell($cell, cell, M.fightinfo.imp.fast);
 			}
 
-			//This shit doesn't work and I don't know why (What is the celltitle??? is it the name of the nature? Imps are labelled Toxic/Gusty/Frozen but that didin't work either)
-			if (cell.name.toLowerCase().indexOf('poison') > -1)				// Poison Token cell
-			{
-				if (cell.special.length === 0)
-					$cell.innerHTML = "<span class=\"glyphicon glyphicon-flask\"></span> ";
-
-				$cell.title = cell.name;
-				//$cell.style.color = M["fightinfo"].colors.exotic; //(This changes the colour of the glyph - bad bc it overrides trimps and looks bad against corruption etc)
-				$cell.style.textShadow = '0px 0px 10px #ffffff';
+			//This shit doesn't work and I don't know why (What is the cell.title??? is it the name of the nature? Imps are labelled Toxic/Gusty/Frozen but that didn't work either)
+			else if (cell.name.toLowerCase().indexOf('poison') > -1) {
+				updateCell($cell, cell, M.fightinfo.imp.poison);
 			}
-			if (cell.name.toLowerCase().indexOf('wind') > -1)				// Wind Token cell
-			{
-				if (cell.special.length === 0)
-					$cell.innerHTML = "<span class=\"icomoon icon-air\"></span> ";
 
-				$cell.title = cell.name;
-				//$cell.style.color = M["fightinfo"].colors.exotic; //(This changes the colour of the glyph - bad bc it overrides trimps and looks bad against corruption etc)
-				$cell.style.textShadow = '0px 0px 10px #ffffff';
+			//Wind Token
+			else if (cell.name.toLowerCase().indexOf('wind') > -1) {
+				updateCell($cell, cell, M.fightinfo.imp.wind);
 			}
-			if (cell.name.toLowerCase().indexOf('ice') > -1)				// Ice Token cell
-			{
-				if (cell.special.length === 0)
-					$cell.innerHTML = "<span class=\"glyphicon glyphicon-certificate\"></span> ";
 
-				$cell.title = cell.name;
-				//$cell.style.color = M["fightinfo"].colors.exotic; //(This changes the colour of the glyph - bad bc it overrides trimps and looks bad against corruption etc)
-				$cell.style.textShadow = '0px 0px 10px #ffffff';
+			//Ice Token
+			else if (cell.name.toLowerCase().indexOf('ice') > -1) {
+				updateCell($cell, cell, M.fightinfo.imp.ice);
 			}
+
+			//Cell Titles
+			$cell.title = cell.name;
+			if (cell.corrupted && cell.corrupted.startsWith("corrupt"))
+				$cell.title += " - " + mutationEffects[cell.corrupted].title;
 		}
 	}
 
