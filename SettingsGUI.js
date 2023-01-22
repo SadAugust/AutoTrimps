3c51114fecfc0c57cdfd172ea9501927eaef4be7
SadAugust <meandalena@gmail.com>
2023-01-22 15:22:42 +0000

    Maybe fix Void Map counter

--- SettingsGUI.js
+++ SettingsGUI.js
@@ -51,6 +51,8 @@ function automationMenuInit() {
 	voidMapContainer.appendChild(voidMapText);
 	var trimpsButtonCol = document.getElementById("trimps");
 	trimpsButtonCol.appendChild(voidMapContainer);
+	document.getElementById('freeVoidMap').parentNode.style.display = 'block';
+	document.getElementById('freeVoidMap').style.display = 'block';
 
 	var $portalTimer = document.getElementById('portalTimer');
 	$portalTimer.setAttribute('onclick', 'toggleSetting(\'pauseGame\')');
