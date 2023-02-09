fe60bbb2503f842db1e96147690bb983e5be7127
SadAugust <meandalena@gmail.com>
2023-02-09 20:03:42 +0000

    Version update

--- AutoTrimps2.js
+++ AutoTrimps2.js
@@ -1,4 +1,4 @@
-var ATversion = 'SadAugust v6.002',
+var ATversion = 'SadAugust v6.003',
 	atscript = document.getElementById('AutoTrimps-script'),
 	basepath = 'https://SadAugust.github.io/AutoTrimps_Local/',
 	modulepath = 'modules/';
