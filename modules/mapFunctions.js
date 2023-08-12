b32bb0aaca2caeeb2a2677ed5ff1ee7bd9293b37
SadAugust <meandalena@gmail.com>
2023-08-12 20:27:35 +0100

    Fix for VM farm spam

--- modules/mapFunctions.js
+++ modules/mapFunctions.js
@@ -497,8 +497,8 @@ function voidMaps() {
 			//Override for if we have already farmed enough maps. Gets reset when Void Map MAZ window is saved.
 			if (defaultSettings.voidFarm && MODULES.mapFunctions.hasVoidFarmed !== (getTotalPortals() + "_" + game.global.world) && (defaultSettings.hitsSurvived > hdStats.hitsSurvivedVoid || defaultSettings.hdRatio < hdStats.vhdRatioVoid)) {
 				//Print farming message if we haven't already started HD Farming for stats.
-				if (!mapSettings.voidFarm)
-					debug('Void Maps (z' + game.global.world + 'c' + (game.global.lastClearedCell + 2) + ') farming for stats before running void maps.', "map_Details");
+				if (!mapSettings.voidFarm && getPageSetting('autoMaps'))
+					debug(mapName + ' (z' + game.global.world + 'c' + (game.global.lastClearedCell + 2) + ') farming for stats before running void maps.', "map_Details");
 				return hdFarm(true, true);
 			}
 		}
