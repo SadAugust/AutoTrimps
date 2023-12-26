332ddd9765334450d4da355866bf6441baf8a22e
Kaili Jiang <7516195+langerest@users.noreply.github.com>
2023-12-25 19:54:49 -0500

    Attempt to fix alchemy not resetting

--- modules/mapFunctions.js
+++ modules/mapFunctions.js
@@ -2569,7 +2569,7 @@ function alchemy(lineCheck) {
         }
     }
 
-    if (mapSettings.mapName === mapName && !shouldMap) {
+    if (!shouldMap) {
         mappingDetails(mapName, mapLevel, mapSpecial, alchObj.potionsOwned[mapSettings.potionIndex], alchObj.potionNames[mapSettings.potionIndex]);
         resetMapVars(setting, settingName);
     }
