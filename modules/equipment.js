a194fb96af5075c2a472674249fd3d7a574e4c5a
Psycho-Ray <alex_sander_r.silva@hotmail.com>
2024-01-30 02:23:34 -0300

    ENH: Saves metal for Miners a bit earlier

--- modules/equipment.js
+++ modules/equipment.js
@@ -317,7 +317,7 @@ function autoEquip() {
 			return false;
 
 		//Saves metal for Miners
-		if (Miners.allowed && !Miners.done) return;
+		if (!challengeActive('Metal') && !Miners.done) return;
 	}
 
 	//If running a wood or metal quest then disable autoEquip
