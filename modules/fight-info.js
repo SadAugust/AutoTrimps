460b28319966c799a8a8291c180480d444ce0459
SadAugust <meandalena@gmail.com>
2024-08-16 14:33:00 +0100

    Improve grid icons - Thanks Shivae

--- modules/fight-info.js
+++ modules/fight-info.js
@@ -11,7 +11,7 @@
 			skel: { icon: '"glyphicon glyphicon-italic"', shadow: '0px 0px 10px #ffffff', color: '#ffffff' },
 			exotic: { icon: '"glyphicon glyphicon-sunglasses"', shadow: '0px 0px 10px #fb753f', color: '#ff0000' },
 			powerful: { icon: '"glyphicon glyphicon-fire"', shadow: '0px 0px 10px #ff0c55', color: '#ff0c55' },
-			fast: { icon: '"glyphicon glyphicon-forward"', shadow: '0px 0px 10px #ffffff', color: '#666666' },
+			fast: { icon: '"glyphicon glyphicon-forward"', shadow: '0px 0px 10px #ffffff', color: '#000000' },
 			poison: { icon: '"glyphicon glyphicon-flask"', shadow: '0px 0px 10px #ffffff', color: '#00ff00' },
 			wind: { icon: '"icomoon icon-air"', shadow: '0px 0px 10px #ffffff', color: '#99ffff' },
 			ice: { icon: '"glyphicon glyphicon-certificate"', shadow: '0px 0px 10px #ffffff', color: '#00ffff' }
@@ -58,29 +58,75 @@
 		return $cell.innerHTML;
 	}
 
-	function updateCellContent($cell, cell) {
-		const cellName = cell.name.toLowerCase();
-		let innerHTML = $cell.innerHTML;
+	function updateCell($cell, special, specialIcon, isFast) {
+		let innerCell = $cell.innerHTML;
+		let parent = $cell;
 
-		if (cellName.includes('skele')) {
-			innerHTML = updateCell($cell, cell, M.fightinfo.imp.skel);
-		} else if (cellName in M['fightinfo'].exotics) {
-			const icon = M.fightinfo.allExoticIcons ? M.fightinfo.exotics[cellName].icon : undefined;
-			innerHTML = updateCell($cell, cell, M.fightinfo.imp.exotic, icon, true);
-		} else if (cellName in M['fightinfo'].powerful) {
-			const icon = M.fightinfo.allPowerfulIcons ? M.fightinfo.powerful[cellName].icon : undefined;
-			innerHTML = updateCell($cell, cell, M.fightinfo.imp.powerful, icon, M.fightinfo.allPowerfulIcons, true);
-		} else if ((M['fightinfo'].fastImps.includes(cell.name) && (!cell.corrupted || !cell.corrupted.startsWith('corrupt'))) || (cell.u2Mutation !== undefined && Object.keys(cell.u2Mutation).length !== 0)) {
-			innerHTML = updateCell($cell, cell, M.fightinfo.imp.fast);
-		} else if (cellName.includes('poison')) {
-			innerHTML = updateCell($cell, cell, M.fightinfo.imp.poison);
-		} else if (cellName.includes('wind')) {
-			innerHTML = updateCell($cell, cell, M.fightinfo.imp.wind);
-		} else if (cellName.includes('ice')) {
-			innerHTML = updateCell($cell, cell, M.fightinfo.imp.ice);
+		if ($cell.children.length >= 1) {
+			parent = $cell.children[0];
+			if (parent.children.length >= 3) return innerCell;
 		}
 
-		return innerHTML;
+		if (special) {
+			$cell.style.color = $cell.style.color;
+			/* $cell.style.textShadow = special.shadow; */
+		}
+
+		const emptyField = '<span title="Empty" class="glyphicon glyphicon-heart-empty" style="visibility: hidden;"></span>';
+
+		if (innerCell.trim() === '&nbsp;') {
+			innerCell = emptyField;
+		}
+
+		let fastIconHtml = '';
+		if (isFast) {
+			const fastIcon = M['fightinfo'].imp.fast;
+			fastIconHtml = `<span title="Fast" class=${fastIcon.icon} style="text-shadow: ${fastIcon.shadow};color: ${fastIcon.color};"></span>`;
+		} else {
+			fastIconHtml = emptyField;
+		}
+
+		let specialIconHtml = '';
+		if (specialIcon) {
+			specialIconHtml = `<span title="${specialIcon.name}" class=${specialIcon.icon} style="text-shadow: ${special.shadow};color: ${$cell.style.color};"></span>`;
+		} else {
+			specialIconHtml = emptyField;
+		}
+
+		innerCell = `
+			<div style="display: flex; align-items: center; justify-content: center;">
+				<div style="width: 40px; text-align: center;">${specialIconHtml}</div>
+				<div>${innerCell}</div>
+				<div style="width: 40px; text-align: center;">${fastIconHtml}</div>
+			</div>
+		`;
+
+		return innerCell;
+	}
+
+	function updateCellContent($cell, cell) {
+		const cellName = cell.name.toLowerCase();
+		const isFast = (M['fightinfo'].fastImps.includes(cell.name) && (!cell.corrupted || !cell.corrupted.startsWith('corrupt'))) || (cell.u2Mutation !== undefined && Object.keys(cell.u2Mutation).length !== 0);
+		let special = null;
+		let specialIcon = null;
+
+		if (cellName.includes('skele')) {
+			special = M.fightinfo.imp.skel;
+		} else if (cellName in M['fightinfo'].exotics) {
+			special = M.fightinfo.imp.exotic;
+			specialIcon = M.fightinfo.allExoticIcons ? M.fightinfo.exotics[cellName] : null;
+		} else if (cellName in M['fightinfo'].powerful) {
+			special = M.fightinfo.imp.powerful;
+			specialIcon = M.fightinfo.allPowerfulIcons ? M.fightinfo.powerful[cellName] : null;
+		} else if (cellName.includes('poison')) {
+			special = M.fightinfo.imp.poison;
+		} else if (cellName.includes('wind')) {
+			special = M.fightinfo.imp.wind;
+		} else if (cellName.includes('ice')) {
+			special = M.fightinfo.imp.ice;
+		}
+
+		return updateCell($cell, special, specialIcon, isFast);
 	}
 
 	function updateCellTitle($cell, cell) {
@@ -152,18 +198,17 @@
 			M['fightinfo'].lastProcessedWorld = game.global.world;
 		}
 
+		const gridElement = game.global.mapsActive ? document.getElementById('mapGrid') : document.getElementById('grid');
 		const rowSource = game.global.mapsActive ? M['fightinfo'].$mapGrid.children : M['fightinfo'].$worldGrid.children;
 		const $rows = Array.prototype.slice.call(rowSource).reverse();
 		const cellArray = $rows.reduce((acc, row) => acc.concat(Array.prototype.slice.call(row.children)), []);
 
 		const updates = processCells(cellArray, []);
-
 		const fragment = createFragment(updates, $rows);
 		const reversedFragment = reverseFragment(fragment);
 
-		const parentElement = game.global.mapsActive ? document.getElementById('mapGrid') : document.getElementById('grid');
-		parentElement.innerHTML = '';
-		parentElement.appendChild(reversedFragment);
+		gridElement.innerHTML = '';
+		gridElement.appendChild(reversedFragment);
 	}
 
 	M['fightinfo'].Update = Update;
