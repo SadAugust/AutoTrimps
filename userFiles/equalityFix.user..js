9630d6b816145b309dcfb2af6425227763f13485
SadAugust <meandalena@gmail.com>
2025-01-22 07:57:28 +0000

    Add equalityFix temp file

---
+++ userFiles/equalityFix.user..js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name		AutoTrimps-SadAugust_gameUpdatesTest
+// @version		1.0-SadAugust
+// @namespace	https://SadAugust.github.io/AutoTrimps
+// @description	Fix minor game bugs & significantly speedup Time Warp!
+// @author		SadAugust, NooNoo, Quia
+// @match		*://trimpstest510.netlify.app/*
+// @connect		*://SadAugust.github.io/AutoTrimps/*
+// @connect		*://trimps.github.io/*
+// @connect		self
+// @grant		GM_xmlhttpRequest
+// ==/UserScript==
+
+function loadScript(id, src) {
+	const script = document.createElement('script');
+	script.id = id;
+	script.src = src;
+	script.setAttribute('crossorigin', 'anonymous');
+	document.head.appendChild(script);
+}
+
+//This can be edited to point to your own Github Repository URL.
+loadScript('AutoTrimps-SadAugust-equalityFixTest', 'https://sadaugust.github.io/AutoTrimps/mods/equalityFix.js');
